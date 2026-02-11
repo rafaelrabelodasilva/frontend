813e6e73a4573e33b972ce8bc2d074e3515f3b27
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2026-02-11 19:14:55 -0300

    Searching for movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,16 +19,31 @@ function Home() {
         console.log(error)
         setError("Failed to load movies...")
       }
-        finally {
-          setLoading(false)
-        }
+      finally {
+        setLoading(false)
       }
+    }
     loadPopularMovies()
   }, [])
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault()
-    alert(searchQuery)
+
+    if (!searchQuery.trim()) return
+    if (loading) return
+
+    setLoading(true)
+
+    try {
+      const searchResults = await searchMovies(searchQuery)
+      setMovies(searchResults)
+      setError(null)
+    } catch (error) {
+      console.log(error)
+      setError("Failed to search movies...")
+    } finally {
+      setLoading(false)
+    }
     setSearchQuery("")
   }
 
@@ -45,14 +60,18 @@ function Home() {
       <button type="submit" className='search-button'>Search</button>
     </form>
 
-    <div className="movies-grid">
+    {error && <div className='error-message'>{error}</div>}
+
+    {loading ? (
+      <div className='loading'>Loading...</div>
+    ) : (<div className="movies-grid">
       {movies.map(
-        (movie) =>
-        (
+        (movie) => (
           <MovieCard movie={movie} key={movie.id} />
-        )
-      )}
+        ))}
     </div>
+    )}
+
   </div>
 }
 
